b1a851f8d31d453ba97212b78a98f76a469b8917
neulhan <neulhan@icloud.com>
2025-04-18 17:07:53 +0900

    Feat : privacy 페이지 레이아웃 추가

---
+++ app/privacy/page.tsx
@@ -0,0 +1,43 @@
+export default function Privacy() {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-3xl mx-auto my-10 overflow-x-hidden">
+      <h1 className="text-2xl font-bold">개인정보 처리방침</h1>
+      <p className="text-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsum
+        asperiores amet mollitia architecto consequatur libero veritatis
+        praesentium repudiandae excepturi animi ab laudantium recusandae,
+        consequuntur veniam. Exercitationem, perferendis quasi autem laudantium
+        odio fuga aspernatur cupiditate rem fugiat, placeat eum aut? Dolorem
+        deserunt explicabo est error ducimus et harum! Inventore reprehenderit
+        dolore nihil cum rem perspiciatis quae dicta eaque amet laudantium eos,
+        excepturi tempora mollitia labore numquam est consequuntur sapiente
+        aliquid ut et saepe distinctio odio! Id a sed deserunt? Reiciendis sunt
+        reprehenderit quod tempore alias, quia enim, autem, eveniet a placeat
+        recusandae magni quam vel voluptatibus illo corporis quisquam repellat
+        consequuntur! Vero illum repellat natus. Quod quia, placeat reiciendis
+        libero similique accusamus deserunt minus nulla eveniet itaque
+        dignissimos nihil sint sapiente necessitatibus cumque ipsa incidunt
+        numquam saepe ab, veritatis totam. Beatae et numquam non assumenda?
+        Repudiandae, ad. Reiciendis eum placeat repellat, atque distinctio
+        maxime facere consequuntur deleniti dolorum tempore laudantium ipsa
+        fugit quisquam quam totam eligendi id tempora dolorem autem nulla in ab
+        voluptatibus obcaecati. Veniam quis optio laudantium reiciendis. Eaque
+        odit placeat molestiae voluptates recusandae enim eveniet iure numquam
+        reprehenderit labore. Repudiandae temporibus nam placeat eveniet vero
+        quidem maiores repellat nulla incidunt assumenda impedit, iusto enim
+        necessitatibus voluptates error corrupti tenetur numquam esse totam
+        deserunt porro dicta. Quidem illum deleniti nam. Perspiciatis eum dolore
+        velit cum amet sequi, accusamus ipsum nemo, error molestias quasi labore
+        nihil alias laudantium, minus totam deserunt veniam ut hic ab!
+        Repudiandae iusto iure totam nihil accusamus eos sequi doloremque nulla,
+        maiores corporis sed. Expedita nam odio laborum, nesciunt vero minima
+        repellendus! Odio aut rerum nihil quos nobis exercitationem incidunt
+        debitis aspernatur, fugit accusantium beatae unde vitae? Temporibus
+        commodi voluptas fuga quidem quae nesciunt non a corrupti numquam
+        consectetur doloremque aliquid, debitis excepturi fugiat omnis.
+        Voluptates aut, illum similique possimus maxime deserunt vel temporibus
+        numquam!
+      </p>
+    </div>
+  );
+}
